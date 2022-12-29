799210162c0755a0e3cf23b14b09776183942653
Lakshay22104054 <121024260+Lakshay22104054@users.noreply.github.com>
2022-12-29 13:52:23 +0530

    Add files via upload

---
+++ Assign 1.py
@@ -0,0 +1,46 @@
+
+
+#q1
+x=int(input("first number"))
+y=int(input("second number"))
+=int(input("third number"))
+d=(x+y+z)/3
+print(d)
+
+#q2
+x = float(input("enter your income"))
+y = x-10000
+z = int(input("dependents"))
+d = z*3000
+e = y-d
+f = (0.2*e)
+print(f)
+
+#q3
+from math import *
+x = float(input("enter value"))
+y = x/60
+ z   = (x%60)
+d = "mins"
+e = floor(y)
+f = "secs"
+print(e,str(d),"and",z,str(f))
+
+#q4
+x = int(input("num1"))
+y = float(input("num2"))
+z= input("num3")
+d = x+y+int(z)
+print(d)
+
+#q5
+import math
+x=0
+while x<=345:
+    print(math.sin(math.radians(x)), math.cos(math.radians(x)))
+    x = x+15
+
+
+
+
+
